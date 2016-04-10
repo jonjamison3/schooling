0962b038c9a0c9df44aa8d5088cfdb4247d014c0
jonjamison3 <jonjamison3@gmail.com>
2016-04-10 17:33:42 -0400

    adding script file

--- php/assignment4/ProductList.php
+++ php/assignment4/ProductList.php
@@ -10,12 +10,26 @@ require('database.php');
 //
 
 include 'header.php';
+
+$query = "select * from products order_by name";
+$products = $db->query($query);
 ?>
 
 <br/>
 <br/>
 <h2>Remove this markup and replace it with a table containing Products 
         data(along with a column for a delete) as shown in the assignment UI</h2>
+
+<table>
+    <?php
+        //foreach($products as $product):
+    ?>
+    <tr><td></td></tr>
+    <?php
+        //endforeach;
+    ?>
+</table>
+
 <br/>
 <br/>
 
