c3d4d06c6261cf83ac4d206149dd97e7dc073cd9
jonjamison3 <jonjamison3@gmail.com>
2016-04-21 12:07:00 -0400

    fleshing out incident addition logic

--- php/assignment4/AddIncident.php
+++ php/assignment4/AddIncident.php
@@ -5,25 +5,89 @@
  */
 
 require('database.php');
+  $customerID  = $_POST['txtCustomerID'];
+  $productCode = $_POST['txtProductCode'];
+  $title  = $_POST['txtTitle'];
+  $description  = $_POST['txtDescription'];
 
-include 'header.php';
-    if (isset($_POST['btnAddProduct'])) {
+    if (isset($_POST['btnAddIncident'])) {
         //gathering input sent over and assigning variables
-        $customerID  = $_POST['txtCustomerID'];
-        $productCode = $_POST['txtProductCode'];
-        $title  = $_POST['txtTitle'];
-        $description  = $_POST['txtDescription'];
-        //try/catch for potential errors
-        try{
-          //constructing a query
-          $query = "insert into incidents values(default, '$customerID', '$productCode',null, NOW(), null, '$title', '$description')";
-          //using the query
-          $db->query($query);
-          //redirecting to listed items
-          header("Location: ./incidentList.php");
-        }catch(Exception $e){
-          //providing message with exception for user
-          echo "Encountered an error with the exception: " . $e.getMessage();
+        $validCustomerID = isValidCustomerID($customerID, $db);
+        $validProductCode = isValidProductCode($productCode, $db);
+        //validate inputs as not empty and valid
+        if($validCustomerID&&!empty($productCode)&&!empty($title)&&!empty($description)){
+          //try/catch for our db work
+          try{
+            //if data is ok add a product to the database
+
+            //construct a new query for insertion
+            $query = "insert into incidents values(default, '$customerID', '$productCode',null, NOW(), null, '$title', '$description')";
+            //executing the query
+            $db->exec($query);
+
+            //redirecting for display of results
+            header("Location: ./incidentList.php");
+          }catch(Exception $e){
+            //here be your error
+            echo "Encountered an exception: ". $e->getMessage();
+          }
+        } else {
+          //if data is NOT ok - show error message and redisplay form
+
+          //create array to catch string values of invalid input
+          $badInfo = [];
+          //check given input for our failure causing culprit(s)
+          badInputCheck($customerID, $badInfo, 'customer ID', $validCustomerID);
+          badInputCheck($productCode, $badInfo, 'product code', $validProductCode);
+          badInputCheck($title, $badInfo, 'title');
+          badInputCheck($description, $badInfo, 'description');
+
+          //create string for display of proper informative error
+          $badInfo = join(", ", $badInfo);
+          $feedback = "Please enter a valid $badInfo and try again";
+          //redirecting to form
+          include "AddIncidentForm.php";
         }
+
     }
+    function badInputCheck($inputName, &$failArray, $failString, $otherwiseValid=true){
+      //if our field is empty going in or not otherwise valid due to optional param
+      if(empty($inputName)||$otherwiseValid===false){
+        //push to our target 'error' array a string of our choosing
+        array_push($failArray, $failString);
+      }
+    }
+    function isValidCustomerID($customerID, $db){
+      //query db for productcode input to ensure it's not a duplicate
+      $query = "SELECT * FROM incidents where customerID='$customerID'";
+      $result = $db->prepare($query);
+      $result->execute();
+
+      //returning number of duplicates by "fetching all" first column values
+      $matchCount = sizeof($result->fetchAll(PDO::FETCH_COLUMN, 0));
+
+      //if a row is actually returned, input is invalid
+      if($matchCount>0){
+        return false;
+      }else{
+        return true;
+      }
+    }
+    function isValidProductCode($productCode, $db){
+      //query db for productcode input to ensure it's not a duplicate
+      $query = "SELECT * FROM products where productCode='$productCode'";
+      $result = $db->prepare($query);
+      $result->execute();
+
+      //returning number of duplicates by "fetching all" first column values
+      $matchCount = sizeof($result->fetchAll(PDO::FETCH_COLUMN, 0));
+
+      //if a row is actually returned, input is valid
+      if($matchCount==0){
+        return false;
+      }else{
+        return true;
+      }
+    }
+
 ?>
--- php/assignment4/AddIncidentForm.php
+++ php/assignment4/AddIncidentForm.php
@@ -2,23 +2,57 @@
 /*
  * This page will edit and add product data to the database.
  */
-
 require('database.php');
-
-include 'header.php';
+include ('header.php');
 ?>
 <form action="AddIncident.php" method="Post">
 
     <br/>
     <br/>
     <h2>Add Incident</h2>
-    <table class="borderless">
-            <tr><td>Customer ID: </td><td><input type="text" name ="txtCustomerID" /></td><td></td></tr>
-            <tr><td>Product Code: </td><td><input type="text" name ="txtProductCode" /></td><td></td></tr>
-            <tr><td>Title: </td><td><input type="text" name ="txtTitle" /></td><td></td></tr>
-            <tr><td>Description: </td><td><textarea rowspan=2 name ="txtDescription"></textarea></td><td></td></tr>
-            <tr><td></td><td><input type="submit" name ="btnAddProduct" value="Add Incident"/></td><td></td></tr>
-        
+<table class="borderless">
+  <span style='color:red;font-style:italic;font-weight:bold;margin-left:5%;font-size:1.1em;'><?php if (isset($feedback)){echo $feedback;} ?></span>
+        <tr>
+          <td>Customer ID: </td>
+          <td>
+            <input type="text" name ="txtCustomerID" value="<?php if (isset($customerID)){echo $customerID;}?>"/>
+          </td>
+          <td>
+          </td>
+        </tr>
+        <tr>
+          <td>Product Code: </td>
+          <td>
+            <input type="text" name ="txtProductCode"  value="<?php if (isset($productCode)){echo $productCode;}?>"/>
+          </td>
+          <td>
+          </td>
+        </tr>
+        <tr>
+          <td>Title: </td>
+          <td>
+            <input type="text" name ="txtTitle" value="<?php if (isset($title)){echo $title;}?>"/>
+          </td>
+          <td>
+          </td>
+        </tr>
+        <tr>
+          <td>Description: </td>
+          <td>
+            <textarea rowspan=2 name ="txtDescription"></textarea>
+          </td>
+          <td>
+          </td>
+        </tr>
+        <tr>
+          <td>
+          </td>
+          <td>
+            <input type="submit" name ="btnAddIncident" value="Add Incident"/>
+          </td>
+          <td>
+          </td>
+        </tr>
     </table>
     <br/>
     <br/>
